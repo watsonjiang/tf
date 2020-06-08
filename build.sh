3b9adf359e77083c729227040b030066c0696178
jiangq <jiangq@xiaopeng.com>
2020-06-08 13:55:06 +0800

    优化build脚本

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@ if ! command -v python3 >/dev/null 2>&1; then
   echo python3 is not found && exit 1
 fi
 
-rm -rf dist tflean.egg-info build
+for f in dist tflearn.egg-info build
+do
+   if [ -d $f ];then
+      echo "clean directory $f"
+      rm -rf $f
+   fi
+done
+
 python3 setup.py bdist_wheel
 
 cp Dockerfile dist/
